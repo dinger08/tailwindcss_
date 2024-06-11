ec11e18644daf7540b6f61e109913bd0163118f7
dinger08 <jonasniikmalm@gmail.com>
2024-06-11 00:12:53 +0000

    first commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+const menu_icon = document.querySelector('#menu_icon');
+const menu = document.querySelector('#menu');
+
+menu_icon.addEventListener('click', (e) => {
+  if (menu.classList.contains('hidden')) {
+    menu.classList.remove('hidden');
+  } else {
+    menu.classList.add('hidden');
+  }
+});
